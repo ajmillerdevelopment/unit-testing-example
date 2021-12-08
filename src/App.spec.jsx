d36b99382ad636406b1afb024f7b3fc5c5493b53
Alex Miller <ajm9594@gmail.com>
2021-12-08 08:51:54 -0800

    more tests

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -1,3 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "./App";
+import vm from "./vm";
+
+it("simple render test of app", () => {
+    render(<App />);
+    expect(screen.getByText("Cow Counter")).toBeInTheDocument();
+});
+
+it("tests add cows", () => {
+    vm.countCow = jest.fn();
+    render(<App />);
+    userEvent.click(screen.getByText("Count Cow"));
+    expect(vm.countCow).toBeCalled();
+    expect(screen.getByText("🐮")).toBeInTheDocument();
+});
